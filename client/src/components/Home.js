dd3e22b810ff4fdd0bdca46bb05fc7deea74b9d0
shreykoradia <shrey.koradia@yahoo.com>
2023-06-16 13:56:12 +0530

    added new cors policy change

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,22 +14,6 @@ const Home = ({socket}) => {
   };
   return (
     <>
-    {/* <form className="home__container" onSubmit={handleSubmit}>
-      <h2 className="home__header">Sign in to UniChat</h2>
-      <label htmlFor="username">Choose a Username</label>
-      <input
-        type="text"
-        minLength={6}
-        name="username"
-        id="username"
-        className="username__input"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <button className="home__cta">SIGN IN</button>
-      <h5 className='founder-name'>Developed by <a href='https://twitter.com/shreykoradia'>SHREY</a></h5>
-      <h6>Visit the Repository on Github @<a href='https://github.com/shreykoradia/UniChat'>UniChat</a>, For working and improving the app contact at the handle above , soon open for OpenSource</h6>
-    </form> */}
     <div className='parent_container'>
     <form className='form_container card_effect' onSubmit={handleSubmit}>
       <h2 className='home_header'>Unichat</h2>
