32300db502e7b86a51e66d4f6831646800e12186
shreykoradia <shrey.koradia@yahoo.com>
2023-06-17 17:12:41 +0530

    fixed es-lint

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 
 const Home = ({socket}) => {
   const navigate = useNavigate();
